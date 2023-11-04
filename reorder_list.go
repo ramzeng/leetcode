99028229fcbd2d4117c127503d41a280b22198ef
曾是然 <srizym@gmail.com>
2023-11-04 15:43:10 +0800

    reorder list

---
+++ reorder_list.go
@@ -0,0 +1,41 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reorderList(head *ListNode) {
+	if head == nil {
+		return
+	}
+
+	var nodes []*ListNode
+	
+	// 借助外部存储，实现下标访问
+	for node := head; node != nil; node = node.Next {
+		nodes = append(nodes, node)
+	}
+
+	// 左右指针
+	left, right := 0, len(nodes)-1
+
+	for left < right {
+		// 交替前进
+		nodes[left].Next = nodes[right]
+		left++
+
+		if left == right {
+			break
+		}
+
+		nodes[right].Next = nodes[left]
+		right--
+	}
+
+	// 不能忘记结束
+	nodes[right].Next = nil
+}
