50f42fb4eeb7eb747a16cfe20a5f274daee24482
ramzeng <srizym@gmail.com>
2024-01-23 22:13:31 +0800

    twoSum

--- 1.两数之和.go
+++ 1.两数之和.go
@@ -6,7 +6,19 @@
 
 // @lc code=start
 func twoSum(nums []int, target int) []int {
+	viewed := make(map[int]int)
 
+	for i := 0; i < len(nums); i++ {
+		v := target - nums[i]
+		if j, ok := viewed[v]; ok {
+			return []int{j, i}
+		} else {
+			viewed[nums[i]] = i
+		}
+	}
+
+	return nil
 }
+
 // @lc code=end
 
