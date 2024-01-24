8cf9dc7ef057c917b1de572ef03cbd152a8b60d2
ramzeng <srizym@gmail.com>
2024-01-24 09:10:51 +0800

    打家劫舍

--- 198.打家劫舍.go
+++ 198.打家劫舍.go
@@ -6,6 +6,37 @@
 
 // @lc code=start
 func rob(nums []int) int {
+	// 朴素做法
+	// var dfs func(i int) int
+
+	// n := len(nums)
+	// cached := make([]int, n)
+
+	// for i, _ := range cached {
+	// 	cached[i] = -1
+	// }
+
+	// dfs = func(i int) int {
+	// 	if i < 0 {
+	// 		return 0
+	// 	}
+
+	// 	if cached[i] != -1 {
+	// 		return cached[i]
+	// 	}
+
+	// 	// 3 = max(dfs(2), dfs(1)+nums[3])
+	// 	// 2 = max(dfs(1), dfs(0)+nums[2])
+	// 	// 1 = max(dfs(0), dfs(-1)+nums[1])
+	// 	// 0 = max(dfs(-1), dfs(-2)+nums[0])
+	// 	answer := max(dfs(i-1), dfs(i-2)+nums[i])
+	// 	cached[i] = answer
+	// 	return answer
+	// }
+
+	// return dfs(len(nums)-1)
+
+	// 递推做法
 	n := len(nums)
 	answers := make([]int, n+2)
 
@@ -15,5 +46,6 @@ func rob(nums []int) int {
 
 	return answers[n+1]
 }
+
 // @lc code=end
 
--- 213.打家劫舍-ii.go
+++ 213.打家劫舍-ii.go
@@ -6,22 +6,56 @@
 
 // @lc code=start
 func rob(nums []int) int {
-	f := func(nums []int, start, end int) int {
-		n := len(nums)
-		answers := make([]int, n+2)
-
-		for i := start; i < end; i++ {
-			answers[i+2] = max(answers[i+1], answers[i]+nums[i])
-		}
-
-		return answers[end+1]
-	}
-
 	n := len(nums)
 
-	return max(nums[0]+f(nums, 2, n-1), f(nums, 1, n))
+	if n == 1 {
+		return nums[0]
+	}
+
+	return max(f(nums[0:n-1]), f(nums[1:n]))
 }
 
+func f(nums []int) int {
+	// 朴素做法
+	// var dfs func(i int) int
+
+	// n := len(nums)
+	// cached := make([]int, n)
+
+	// for i, _ := range cached {
+	// 	cached[i] = -1
+	// }
+
+	// dfs = func(i int) int {
+	// 	if i < 0 {
+	// 		return 0
+	// 	}
+
+	// 	if cached[i] != -1 {
+	// 		return cached[i]
+	// 	}
+
+	// 	// 3 = max(dfs(2), dfs(1)+nums[3])
+	// 	// 2 = max(dfs(1), dfs(0)+nums[2])
+	// 	// 1 = max(dfs(0), dfs(-1)+nums[1])
+	// 	// 0 = max(dfs(-1), dfs(-2)+nums[0])
+	// 	answer := max(dfs(i-1), dfs(i-2)+nums[i])
+	// 	cached[i] = answer
+	// 	return answer
+	// }
+
+	// return dfs(len(nums)-1)
+
+	// 递推做法
+	n := len(nums)
+	answers := make([]int, n+2)
+
+	for i := 0; i < n; i++ {
+		answers[i+2] = max(answers[i+1], answers[i]+nums[i])
+	}
+
+	return answers[n+1]
+}
 
 // @lc code=end
 
