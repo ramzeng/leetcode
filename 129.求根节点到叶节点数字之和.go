57e28bb272d4f94b924cc196e099bc095b652200
ramzeng <srizym@gmail.com>
2024-01-25 09:10:40 +0800

    practice

---
+++ 129.求根节点到叶节点数字之和.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=129 lang=golang
+ *
+ * [129] 求根节点到叶节点数字之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumNumbers(root *TreeNode) int {
+	var dfs func(node *TreeNode, sum int) int
+	dfs = func(node *TreeNode, sum int) int {
+		if node == nil {
+			return 0
+		}
+
+		sum = sum*10 + node.Val
+
+		if node.Left == node.Right {
+			return sum
+		}
+
+		return dfs(node.Left, sum) + dfs(node.Right, sum)
+	}
+
+	return dfs(root, 0)
+}
+
+// @lc code=end
+
